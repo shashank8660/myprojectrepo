2221054ae8ca3795c7207fd10f54219718ddac21
shashank8660 <80061339+shashank8660@users.noreply.github.com>
2021-03-22 18:32:52 +0530

    Update Calculator.java

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -10,5 +10,10 @@ public class Calculator {
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	
+	public static void add2(int num3,int num4){
+	      System.out.println(num3+num4);
+	  }
+
 
 }
